f9e3e3855f60e5893507914339e55aaa6fa15721
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-11 17:00:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Deciduous extends Tree {
   
   static definition() { 
     return (
-      super.definition () + `Deciduous trees shed their leaves annually.`
+      super.definition () + 'Deciduous trees shed their leaves annually.'
       );
     }
   }
